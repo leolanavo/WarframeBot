bb1ccd09f5baae515ce086973a25ac722961672b
Leonardo Lana <leolanavo@gmail.com>
2017-11-21 17:59:52 -0200

    Add twitter API keys

--- twtter_wrapper.rb
+++ twtter_wrapper.rb
@@ -1,10 +1,10 @@
 class Twitts
     def initialize()
         @client = Twitter::REST::Client.new do |config|
-            config.consumer_key        = "YOUR_CONSUMER_KEY"
-            config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-            config.access_token        = "YOUR_ACCESS_TOKEN"
-            config.access_token_secret = "YOUR_ACCESS_SECRET"
+            config.consumer_key        = "pCaeWkIwqnqtVdKSpcWB2EoPJ"
+            config.consumer_secret     = "tN4nLwBFYlgyiYNKo5JAQ3FX0z7gtcn6nJ85U7aVO61ae6y613"
+            config.access_token        = "1069161786-6NYf1HcZGxByOFZ9xofguBR9dVy1Ng1Gn1SXIx6"
+            config.access_token_secret = "xD1wZM6YmaG5UlWODjuf7p9rSo2kyXmzHMTVKoCDkQlML"
         end
     end
 end
