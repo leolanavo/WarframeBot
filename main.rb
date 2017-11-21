9af4fd548953bbc8b7677230d2f16321bcc5877d
Leonardo Lana <leolanavo@gmail.com>
2017-11-21 17:56:34 -0200

    Add new dummy commands

--- main.rb
+++ main.rb
@@ -9,6 +9,14 @@ Telegram::Bot::Client.run(token) do |bot|
             bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
         when '/stop'
             bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        when '/filter'
+            bot.api.send_message(chat_id: message.chat.id, text: "Filter, #{message.from.fist_name}")
+        when '/alerts'
+            bot.api.send_message(chat_id: message.chat.id, text: "Alerts, #{message.from.fist_name}")
+        when '/invasions'
+            bot.api.send_message(chat_id: message.chat.id, text: "Invasions, #{message.from.fist_name}")
+        when '/sortie'
+            bot.api.send_message(chat_id: message.chat.id, text: "Sortie, #{message.from.fist_name}")
         end
     end
 end
