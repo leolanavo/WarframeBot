681a5e6dd1170f6739a66988b7cb0e15950a232c
Leonardo Lana <leolanavo@gmail.com>
2017-11-21 23:53:11 -0200

    Remove type from Alert and add new fields to Invasion

--- alert.rb
+++ alert.rb
@@ -1,20 +1,21 @@
 class Alert
-    def initialize(mission, planet, credits, reward, time, type)
+    def initialize(mission, planet, credits, reward, time)
         @mission = mission
         @planet = planet
         @credits = credits
         @reward = reward
         @time = time
-        @type = type
     end
 end
 
 class Invasion
-    def initialize(mission, planet, faction, reward)
+    def initialize(mission, planet, f_faction, s_faction, f_reward, s_faction)
         @mission = mission
         @planet = planet
-        @faction = faction
-        @reward = reward
+        @f_faction = f_faction
+        @s_faction = s_faction
+        @f_reward = f_reward
+        @s_reward = s_reward
     end
 end
 
