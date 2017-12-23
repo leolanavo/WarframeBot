a2ac2cee5261d3dca67e499f34449f712292e8c1
Leonardo Lana <leolanavo@gmail.com>
2017-12-23 14:49:15 -0200

    Add the resource menu functionality

--- src/interactions.rb
+++ src/interactions.rb
@@ -1,3 +1,31 @@
-def filter(filter, tw_wrapper)
-    alerts = tw_wrapper.getAlerts()
+RESOURCES = ['Alloy Plate', 'Argon Crystal', 'Circuits', 'Control Module',
+             'Cryotic', 'Ferrite', 'Gallium', 'Morphics', 'Nano Spores',
+             'Neural Sensors', 'Neurodes', 'Orokin Cell', 'Oxium', 'Plastids',
+             'Polymer Bundle', 'Rubedo', 'Salvage', 'Tellurium',
+             'Nitain Extract', 'Synthula']
+
+def create_inline_entry(name, callback_value)
+    Telegram::Bot::Types::InlineKeyboardButton.new(text: name, url: callback_value)
+end
+
+def resource_menu()
+    kb = Array.new
+    i = 0
+    until i > RESOURCES.size do
+        kb.push([create_inline_entry(RESOURCES[i], 'https://google.com'), create_inline_entry(RESOURCES[i+1], 'https://google.com')])
+        i += 2
+    end
+    return kb
+end
+
+def help()
+    "/start \t Start the bot\n" +
+    "/stop \t Stop the bot\n" +
+    "/alerts \t Get the active alerts\n" +
+    "/invasions \t Get the active invasions\n" +
+    "/sortie \t Get the active sortie\n" +
+    "/add \t Add a new filter\n" +
+    "/remove \t Remove a filter\n" +
+    "/get \t Get the active filters\n" +
+    "/help \t Print this menu"
 end
--- src/main.rb
+++ src/main.rb
@@ -10,38 +10,64 @@ wrapper = TWrapper.new()
 user = User.new
 
 Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
-        case message.text.split(" ")[0]
+    bot.listen do |msg|
+        case msg.text.split(" ")[0]
 
         when '/start'
-            user.change_id(message.from.id)
-            bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}," +
-                                 "please add your filters by typing /add <ITEM>")
+            user.change_id(msg.from.id)
+            bot.api.send_message(chat_id: msg.chat.id, text: "Hello, #{msg.from.first_name}")
 
         when '/stop'
-            bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+            bot.api.send_message(chat_id: msg.chat.id, text: "Bye, #{msg.from.first_name}")
 
         when '/add'
-            filter = message.text.split(" ")[1]
+            filter = msg.text.split(" ")[1]
             user.add_filter(filter)
-            bot.api.send_message(chat_id: message.chat.id, text: "#{filter} added to the list")
+            bot.api.send_message(chat_id: msg.chat.id, text: "#{filter} added to the list")
 
         when '/remove'
-            filter = message.text.split(" ")[1]
+            filter = msg.text.split(" ")[1]
             user.remove_filter(filter)
-            bot.api.send_message(chat_id: message.chat.id, text: "#{filter} remove to the list")
+            bot.api.send_message(chat_id: msg.chat.id, text: "#{filter} remove to the list")
 
         when '/get'
-            bot.api.send_message(chat_id: message.chat.id, text: user.get_filters)
+            bot.api.send_message(chat_id: msg.chat.id, text: user.get_filters)
 
         when '/alerts'
-            bot.api.send_message(chat_id: message.chat.id, text: wrapper.get_alerts)
+            bot.api.send_message(chat_id: msg.chat.id, text: wrapper.get_alerts)
 
         when '/invasions'
-            bot.api.send_message(chat_id: message.chat.id, text: wrapper.get_invasions)
+            bot.api.send_message(chat_id: msg.chat.id, text: wrapper.get_invasions)
 
         when '/sortie'
-            bot.api.send_message(chat_id: message.chat.id, text: wrapper.get_sortie)
+            bot.api.send_message(chat_id: msg.chat.id, text: wrapper.get_sortie)
+
+        when '/help'
+            bot.api.send_message(chat_id: msg.chat.id, text: help)
+
+        when '/test'
+            R = ['Alloy Plate', 'Argon Crystal', 'Circuits', 'Control Module',
+                 'Cryotic', 'Ferrite', 'Gallium', 'Morphics', 'Nano Spores',
+                 'Neural Sensors', 'Neurodes', 'Orokin Cell', 'Oxium', 'Plastids',
+                 'Polymer Bundle', 'Rubedo', 'Salvage', 'Tellurium',
+                 'Nitain Extract', 'Synthula']
+
+            i = 0
+            kb1 = Array.new
+            until i == RESOURCES.size() do
+                cell = [
+                    create_inline_entry(R[i],'https://google.com'),
+                    create_inline_entry(R[i+1], 'https://google.com')
+                ]
+                i += 2
+                kb1.push(cell)
+            end
+
+
+            puts "kb1\n" + kb1.to_s + "\n\n\n"
+
+            markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb1)
+            bot.api.send_message(chat_id: msg.chat.id, text: "Filters", reply_markup: markup)
         end
     end
 end
